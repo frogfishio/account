45483b7fdf87c90907c95293bbba6193104ae2e7
El Diablo <eldiablolives@live.com>
2019-10-04 05:26:43 +0000

    cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,3 @@
-/**
- * Created by eldiablo on 01/12/14.
- */
-
-// var gulp = require('gulp');
-// var ts = require('gulp-typescript');
-
-// gulp.task('dist', ['dist-compile', 'dist-files'], function() {
-//     var fs = require('fs');
-//     var pkg = require('./package');
-//     delete pkg.devDependencies;
-//     delete pkg.scripts;
-
-//     require('mkdirp')('./build/release/');
-
-//     fs.writeFileSync('./build/release/package.json', JSON.stringify(pkg, null, 2));
-// });
-
-// gulp.task('dist-files', function() {
-//   gulp.src('src/services/*.yaml').pipe(gulp.dest('build/release/services'));
-// });
-
-// gulp.task('prepare-debug', function() {
-//   var fs = require('fs');
-//   gulp.src('src/services/*').pipe(gulp.dest('build/debug/services'));
-//   gulp.src('test/support/*').pipe(gulp.dest('build/debug/support'));
-// });
-
 const { src, dest, series } = require('gulp');
 
 async function clean() {
